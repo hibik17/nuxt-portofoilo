66594d6f8d2faebc89f2ab52e3a11886f1c2e22b
hibik17 <hbtatsu@gmail.com>
2023-06-04 13:05:51 +0900

    add content text

--- components/Content.vue
+++ components/Content.vue
@@ -4,16 +4,34 @@ import { VueTyper } from "vue-typer";
 
 <template>
   <div class="text-white">
-    <!-- <h1 class="font-bold text-center font-mono text-5xl"> -->
-    <div class="w-full">
+    <div class="w-[70%] m-auto">
       <vue-typer
         text="NICE TO MEET YOU I'M HIBIKI"
-        class="flex justify-center text-5xl font-mono typed typed typed"
+        class="flex justify-start text-5xl font-mono typed typed typed mt-10 mb-10"
         repeat="3"
         caret-animation="smooth"
       ></vue-typer>
+      <p class="text-white text-2xl font-thin mb-10">
+        Your friendly neighborhood frontend developer, UX architect, and
+        JavaScript engineer. I spend my days (and often nights) painting the
+        Internet canvas with <span class="font-bold">Projects</span> and lines
+        of code, turning zeroes and ones into immersive, interactive
+        experiences,
+      </p>
+      <p class="text-white text-2xl font-thin mb-10">
+        Bona fide photochromic <span class="font-bold">Lens</span> enthusiast -
+        sunlight or indoors, I've got it covered. I tread the path of
+        minimalism, finding beauty in simplicity and order. When I'm not
+        crafting beautiful web experiences, you can find me reading
+        <span class="font-bold">Articles</span> or swaying to the rhythm of Pop
+        Music & Jazz, losing myself in the captivating flow of melodies. anyways
+        you can <span class="font-bold">Contact Me</span>
+      </p>
+      <div class="flex">
+        <p>See more about me</p>
+        <p>→</p>
+      </div>
     </div>
-    <!-- </h1> -->
   </div>
 </template>
 
