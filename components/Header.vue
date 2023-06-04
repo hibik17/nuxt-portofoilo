78e56c781b89e82c71da3e46d3f736987fec58a3
hibik17 <hbtatsu@gmail.com>
2023-06-04 12:34:43 +0900

    change img color to white

--- components/Header.vue
+++ components/Header.vue
@@ -3,10 +3,10 @@
 <template>
   <div class="w-[80%] m-auto h-32 flex mb-10">
     <nuxt-link to="/" class="m-6 w-auto">
-      <img src="../src/images/diamond.png" class="w-10 h-10" />
+      <img src="../src/images/diamond.png" class="w-10 h-10 white_img" />
     </nuxt-link>
     <div class="m-6 w-full flex justify-end">
-      <ul class="flex text-slate-600 font-bold align-middle">
+      <ul class="flex text-white font-bold align-middle">
         <li class="mr-6 leading-10">About</li>
         <li class="mr-6 leading-10">Projects</li>
         <li class="mr-6 leading-10">Contact</li>
@@ -14,7 +14,7 @@
           <img
             src="../src/images/dark-mode.png"
             alt="dark mode"
-            class="w-10 h-10"
+            class="w-10 h-10 white_img"
           />
         </li>
       </ul>
